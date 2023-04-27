9b44025969cf11d5304753ce2d8660346284d48a
Jen <73888356+berryjen@users.noreply.github.com>
2023-04-27 17:15:36 +0200

    made website more responsive to device

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import "bootstrap/dist/css/bootstrap.css";
-<<<<<<< Updated upstream
-=======
-import "./style.css";
->>>>>>> Stashed changes
 
 import CurrentWeather from "./CurrentWeather";
 import Footer from "./Footer";
@@ -17,7 +13,3 @@ export default function App() {
     </div>
   );
 }
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
